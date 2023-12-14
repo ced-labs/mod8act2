155588a011736650e7bc0d7a6f9e3dbe9c677c7a
ced-labs <cescribanod@gmail.com>
2023-12-15 00:30:03 +0100

    Collisions test name fixed

--- src/server/collisions.test.js
+++ src/server/collisions.test.js
@@ -1,6 +1,6 @@
 const applyCollisions = require('./collisions');
 const Constants = require('../shared/constants');
-const Player = require('./players');
+const Player = require('./player');
 const Bullet = require('./bullet');
 
 describe('applyCollisions', () => {
